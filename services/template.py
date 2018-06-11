ba9793d2c0b1076eb4c6d776c894b0ec20c6d6e6
Jordan Bermudez <jordan.bermudez@caci.com>
2018-06-11 16:41:56 -0400

    Dynamically import all the files in the services folder

--- services/template.py
+++ services/template.py
@@ -1,11 +1,11 @@
 """
 The following code is the only mandatory code for a service test
-"""
+
 
 # This is needed if you want to log any errors
 import logging
 # This needs to be imported so the decorator and data can be used in the service test
-from settings import data, collect
+from utils.settings import data, collect
 
 # This needs to be above your function so the decorator can add it to a list for functional testing
 @collect(data.get('services').get('YOUR_SERVICE_NAME_YOU_CREATED_IN_THE_JSON_FILE').get('enabled'))
@@ -25,3 +25,4 @@ def A_NAME_THAT_PERTAINS_TO_YOUR_SERVICE_TEST():
     # Have logic in your code to determine if the service is working
     return 0 # Return a 0 if it fails
     return 1 # Return a 1 if it passes
+"""
